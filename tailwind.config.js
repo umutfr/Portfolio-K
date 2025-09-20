8339687895c69c4894545a185c7b30036dd5cae0
DoomLaptop <umutw4141@gmail.com>
2025-09-20 23:21:29 +0300

    Bitti.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
+  safelist: ["block", "hidden", "flex", "grid"], // dinamik class’lar için
   theme: { extend: {} },
   plugins: [],
 }
