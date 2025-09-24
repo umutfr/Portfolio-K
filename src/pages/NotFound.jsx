84bea4a0340899aedeeea74f2dadf45fc47d5e39
DoomLaptop <umutw4141@gmail.com>
2025-09-24 16:57:11 +0300

    some changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
+import { StarBackgorund } from "@/components/StarBackground"
 export const NotFound = () => {
   return (
-    <div className="text-3xl font-bold underline">
-        404 Not Found
-    </div>
-  )
+    <div className="min-h-screen bg-black text-foreground overflow-x-hidden">
+  <StarBackgorund />
+  <div className="min-h-screen flex flex-col items-center justify-center text-white">
+    <h1 className="text-6xl font-bold mb-4"><span className="text-primary">4</span>0<span className="text-primary">4</span></h1>
+    <p className="text-xl mb-6">Sayfa bulunamadı.</p>
+    <a href="/" className="cosmic-button text-black">Ana Sayfaya Dön</a>
+  </div>
+</div>
+  );
 }
