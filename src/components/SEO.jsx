a24a74d55a99a394b8a3ecef18b87506d99daa12
DoomLaptop <umutw4141@gmail.com>
2025-10-24 15:54:54 +0300

    some changes

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -9,6 +9,8 @@ export const SEO = ({ title, description, keywords, url, image }) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       {image && <meta property="og:image" content={image} />}
+      {image && <meta property="og:image:width" content="1200" />}
+      {image && <meta property="og:image:height" content="630" />}
       {url && <meta property="og:url" content={url} />}
       <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
